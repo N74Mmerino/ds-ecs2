e0b3a7c6829d233b46483898832b51e0ad434698
N74Mmerino <nnicolasmmerino@gmail.com>
2024-05-03 22:15:59 -0300

    primercommit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+if (!isset($_GET['y1']) || !isset($_GET['y2']) || !isset($_GET['y3']) || !isset($_GET['y4']) || !isset($_GET['y5'])) {
+echo 'Debe indicar todos los años, del 1 al 5';
+
+} else {
+
+}
